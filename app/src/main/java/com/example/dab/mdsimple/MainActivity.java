8dde921afc26de0da5165bd0f9af53369eeb0f69
dab <wjsdab@163.com>
2017-07-04 15:38:06 +0800

    环境配置

--- app/src/main/java/com/example/dab/mdsimple/MainActivity.java
+++ app/src/main/java/com/example/dab/mdsimple/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.dab.mdsimple;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.dab.mdsimple.kotlin.FloatingActionButtonActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void floatingActionButton(View view) {
+        startActivity(new Intent(this, FloatingActionButtonActivity.class));
     }
 }
---
+++ app/src/main/java/com/example/dab/mdsimple/kotlin/FloatingActionButtonActivity.kt
@@ -0,0 +1,14 @@
+package com.example.dab.mdsimple.kotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+import com.example.dab.mdsimple.R
+
+class FloatingActionButtonActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_floating_action_button)
+    }
+}
