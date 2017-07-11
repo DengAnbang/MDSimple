2b5de58c0840b4e9747b931a61ea105cc9ddd67a
dab <wjsdab@163.com>
2017-07-11 13:56:12 +0800

    base

--- app/src/main/java/com/example/dab/mdsimple/MainActivity.java
+++ app/src/main/java/com/example/dab/mdsimple/MainActivity.java
@@ -1,21 +1,34 @@
 package com.example.dab.mdsimple;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.example.dab.mdsimple.kotlin.FloatingActionButtonActivity;
+import com.example.dab.mdsimple.kotlin.activity.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends BaseActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getContentView() {
+        return R.layout.activity_main;
     }
 
     public void floatingActionButton(View view) {
         startActivity(new Intent(this, FloatingActionButtonActivity.class));
+    }
+
+    @Override
+    public void initToolbarEvent(@Nullable View view, @Nullable Toolbar toolbar) {
+        super.initToolbarEvent(view, toolbar);
+        if (toolbar != null) {
+            toolbar.setTitle("标题");
+            toolbar.setSubtitle("小标题");
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/dab/mdsimple/kotlin/activity/BaseActivity.kt
@@ -0,0 +1,57 @@
+package com.example.dab.mdsimple.kotlin.activity
+
+import android.os.Bundle
+import android.support.annotation.LayoutRes
+import android.support.design.widget.FloatingActionButton
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewStub
+import com.example.dab.mdsimple.R
+
+/**
+ * Created by dab on 2017/7/10.
+ */
+abstract class BaseActivity : AppCompatActivity() {
+    var FAB:FloatingActionButton?=null
+    @LayoutRes
+    protected abstract fun getContentView(): Int
+
+    open fun initView(view: View?) {}
+    open fun initData() {}
+    open fun initEvent() {}
+
+    @LayoutRes
+    open fun getToolbarResID(): Int = 0
+
+    open fun initToolbarEvent(view: View?, toolbar: Toolbar?) {
+        view?.findViewById(R.id.toolbar_back)?.setOnClickListener { finish() }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val inflate = LayoutInflater.from(this).inflate(R.layout.activity_base, null)
+        val viewStub = inflate.findViewById(R.id.vs_content) as ViewStub?
+        val inflateView = inflateView(viewStub!!, getContentView())
+        setContentView(inflate)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar?
+        val toolbarViewStub = toolbar?.findViewById(R.id.toolbar_layout) as ViewStub?
+        val toolbarView = inflateView(toolbarViewStub!!, getToolbarResID())
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        FAB = findViewById(R.id.fab) as FloatingActionButton
+        initView(inflateView)
+        initToolbarEvent(toolbarView, toolbar)
+        initEvent()
+        initData()
+    }
+
+    fun inflateView(viewStub: ViewStub, @LayoutRes layoutResource: Int): View? {
+        if (layoutResource == 0) return null
+        viewStub.layoutResource = layoutResource
+        return viewStub.inflate()
+    }
+
+
+}
